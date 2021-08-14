1d2403f5baac13abdcbae6cb59f7386136edfefc
Mathias Brodala <mbrodala@pagemachine.de>
2021-08-14 15:49:41 +0200

    [TASK] Drop management fields from SQL schema
    See https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/9.3/Feature-85160-AutoCreateManagementDBFieldsFromTCACtrl.html

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,19 +1,6 @@
-#
-# Table structure for table 'tx_formlog_entries'
-#
 CREATE TABLE tx_formlog_entries (
-  uid int(11) unsigned NOT NULL auto_increment,
-  pid int(11) unsigned DEFAULT '0' NOT NULL,
-
-  tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-  crdate int(11) unsigned DEFAULT '0' NOT NULL,
-  deleted int(11) unsigned DEFAULT '0' NOT NULL,
-
   language int(11) unsigned DEFAULT '0' NOT NULL,
   identifier varchar(255) DEFAULT '' NOT NULL,
   data mediumtext,
   finisher_variables mediumtext,
-
-  PRIMARY KEY (uid),
-  KEY parent (pid)
 );

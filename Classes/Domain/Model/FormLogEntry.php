eed40749c0df88cd5b614bc497e8e4be51703dda
Mathias Brodala <mbrodala@pagemachine.de>
2021-08-17 15:03:14 +0200

    [TASK] Add JSON data wrapper
    This encapsulates JSON parsing and exposes the parsed data as array-ish
    object.
    
    Raw data from the database is passed in as constructor argument by the
    Extbase DataMapper thanks to the core TypeInterface. The same interface
    could be used to format updated data for persistence but this is not
    used here.

--- Classes/Domain/Model/FormLogEntry.php
+++ Classes/Domain/Model/FormLogEntry.php
@@ -7,6 +7,7 @@ namespace Pagemachine\Formlog\Domain\Model;
  * This file is part of the Pagemachine TYPO3 Formlog project.
  */
 
+use Pagemachine\Formlog\Domain\Data\JsonData;
 use Pagemachine\Formlog\Domain\Model\FormLogEntry\Language;
 use Pagemachine\Formlog\Domain\Model\FormLogEntry\Page;
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
@@ -24,41 +25,7 @@ class FormLogEntry extends AbstractEntity
 
     public string $identifier = '';
 
-    /**
-     * @var string
-     */
-    protected string $data = '';
+    public ?JsonData $data = null;
 
-    /**
-     * Runtime cache for loaded data
-     */
-    protected ?array $cachedData = null;
-
-    public function getData(): array
-    {
-        if (!is_array($this->cachedData)) {
-            $this->cachedData = json_decode($this->data, true) ?: [];
-        }
-
-        return $this->cachedData;
-    }
-
-    /**
-     * @var string
-     */
-    protected string $finisherVariables = '';
-
-    /**
-     * Runtime cache for loaded finisher variables
-     */
-    protected ?array $cachedFinisherVariables = null;
-
-    public function getFinisherVariables(): array
-    {
-        if (!is_array($this->cachedFinisherVariables)) {
-            $this->cachedFinisherVariables = json_decode($this->finisherVariables, true) ?: [];
-        }
-
-        return $this->cachedFinisherVariables;
-    }
+    public ?JsonData $finisherVariables = null;
 }

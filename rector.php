f826a7309e8c25d18dcfeea2217544a5729f6c70
Mathias Brodala <mbrodala@pagemachine.de>
2023-10-11 15:07:09 +0200

    [TASK] Add Rector configuration

---
+++ rector.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Set\ValueObject\LevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/Classes',
+        __DIR__ . '/Tests',
+    ]);
+
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_80,
+        PHPUnitSetList::PHPUNIT_91,
+    ]);
+
+    $rectorConfig->skip([
+        \Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector::class,
+        \Rector\Php71\Rector\FuncCall\CountOnNullRector::class,
+    ]);
+};

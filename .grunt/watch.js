1897550bf033d6a10d4fc54fbda7d0a10d82a423
Mathias Brodala <mbrodala@pagemachine.de>
2025-02-25 15:49:26 +0100

    [TASK] Drop obsolete Grunt tasks

--- .grunt/watch.js
+++ .grunt/watch.js
@@ -1,14 +1,4 @@
 module.exports = {
-  bower: {
-    files: 'bower.json',
-    tasks: [
-      'clean:css',
-      'clean:js',
-      'bower',
-      'umd',
-      'uglify',
-    ],
-  },
   less: {
     files: '<%= paths.resources.private %>Less/**/*.less',
     tasks: 'less',

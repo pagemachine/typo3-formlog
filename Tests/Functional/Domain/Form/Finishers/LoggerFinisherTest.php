748b72ed44ba13057726735ce68336530ad8fb72
Mathias Brodala <mbrodala@pagemachine.de>
2025-03-03 09:58:53 +0100

    [TASK] Use subrequest for functional LoggerFinisher tests
    Use a regular subrequest to fetch and submit forms with data.
    
    This allows us to drop all kinds of mocking and fake environment setup.
    Also we do not need to know about the internals of the Form framework
    anymore.
    
    The hardest part was testing file uploads which evidently do not work
    with stream-based uploaded files. When using the StreamFactoryInterface
    to create one and "upload" such a file, Extbase/Form will not accept
    this file since there is no "tmp_name" internally. This only works with
    phyisically created temporary files just like we did before.

--- Tests/Functional/Domain/Form/Finishers/LoggerFinisherTest.php
+++ Tests/Functional/Domain/Form/Finishers/LoggerFinisherTest.php
@@ -7,22 +7,10 @@ namespace Pagemachine\Formlog\Tests\Functional\Domain\Form\Finishers;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use TYPO3\CMS\Core\Configuration\SiteConfiguration;
-use TYPO3\CMS\Core\Context\Context;
-use TYPO3\CMS\Core\Core\SystemEnvironmentBuilder;
-use TYPO3\CMS\Core\Http\ServerRequestFactory;
-use TYPO3\CMS\Core\Routing\PageArguments;
-use TYPO3\CMS\Core\Site\SiteFinder;
+use TYPO3\CMS\Core\Http\UploadedFile;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Mvc\ExtbaseRequestParameters;
-use TYPO3\CMS\Extbase\Mvc\Request as ExtbaseRequest;
-use TYPO3\CMS\Extbase\Security\Cryptography\HashService;
-use TYPO3\CMS\Form\Domain\Factory\ArrayFormFactory;
-use TYPO3\CMS\Form\Domain\Model\FormDefinition;
-use TYPO3\CMS\Form\Domain\Runtime\FormRuntime\FormSession;
-use TYPO3\CMS\Form\Domain\Runtime\FormState;
-use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
-use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
-use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+use TYPO3\CMS\Form\Tests\Functional\Framework\FormHandling\FormDataFactory;
+use TYPO3\TestingFramework\Core\Functional\Framework\Frontend\InternalRequest;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
 /**
@@ -39,11 +27,13 @@ final class LoggerFinisherTest extends FunctionalTestCase
         'typo3conf/ext/formlog',
     ];
 
+    protected array $pathsToLinkInTestInstance = [
+        'typo3conf/ext/formlog/Tests/Functional/Domain/Form/Finishers/Fixtures/FormDefinitions' => 'fileadmin/form_definitions',
+    ];
+
     protected function setUp(): void
     {
         parent::setUp();
-
-        $GLOBALS['TYPO3_CONF_VARS']['BE']['lockRootPath'] = ORIGINAL_ROOT;
 
         $this->getConnectionPool()->getConnectionForTable('pages')->insert('pages', ['uid' => 123]);
         $this->setUpFrontendRootPage(123, [
@@ -52,26 +42,6 @@ final class LoggerFinisherTest extends FunctionalTestCase
 
         $siteConfiguration = GeneralUtility::makeInstance(SiteConfiguration::class);
         $siteConfiguration->createNewBasicSite('123', 123, 'http://localhost/');
-
-        $_SERVER['HTTP_HOST'] = 'localhost';
-        $site = GeneralUtility::makeInstance(SiteFinder::class)->getSiteByRootPageId(123);
-        $siteLanguage = $site->getLanguageById(0);
-        $frontendUser = GeneralUtility::makeInstance(FrontendUserAuthentication::class);
-        $frontendUser->initializeUserSessionManager();
-
-        $GLOBALS['TSFE'] = GeneralUtility::makeInstance(
-            TypoScriptFrontendController::class,
-            GeneralUtility::makeInstance(Context::class),
-            $site,
-            $siteLanguage,
-            new PageArguments(123, '0', []),
-            $frontendUser,
-        );
-
-        $requestFactory = GeneralUtility::makeInstance(ServerRequestFactory::class);
-        $request = $requestFactory->createServerRequest('get', 'http://localhost');
-
-        $GLOBALS['TSFE']->determineId($request);
     }
 
     protected function tearDown(): void
@@ -81,74 +51,80 @@ final class LoggerFinisherTest extends FunctionalTestCase
 
     #[DataProvider('formData')]
     #[Test]
-    public function logsSubmittedFormData(array $fields, array $formValues, string $expectedData): void
-    {
-        $formDefinition = $this->buildFormDefinition([
-            'renderables' => [
-                'page' => [
-                    'renderables' => $fields,
-                ],
-            ],
-            'finishers' => [
-                [
-                    'identifier' => 'LogFormData',
-                ],
-            ],
-        ]);
+    public function logsSubmittedFormData(
+        string $formPersistenceIdentifier,
+        string $formIdentifier,
+        array $formValues,
+        array $uploadedFiles,
+        string $expectedData,
+    ): void {
+        $formContentUid = $this->createFormContentElement($formPersistenceIdentifier);
+        $formIdentifier = sprintf('%s-%d', $formIdentifier, $formContentUid);
 
-        $this->submitForm($formDefinition, $formValues);
+        $pageRequest = (new InternalRequest())->withPageId(123);
+        $response = $this->executeFrontendSubRequest($pageRequest);
+
+        self::assertSame(200, $response->getStatusCode());
+
+        $formData = (new FormDataFactory())->fromHtmlMarkupAndXpath((string)$response->getBody(), '//form[@id="' . $formIdentifier . '"]');
+
+        foreach ($formValues as $identifier => $value) {
+            $formData = $formData->with($identifier, $value);
+        }
+
+        $formSubmitRequest = $formData->toPostRequest($pageRequest);
+
+        if (!empty($uploadedFiles)) {
+            $formSubmitRequest = $formSubmitRequest->withUploadedFiles([
+                'tx_form_formframework' => [
+                    $formIdentifier => $uploadedFiles,
+                ],
+            ]);
+        }
+
+        $response = $this->executeFrontendSubRequest($formSubmitRequest);
+
+        self::assertSame(200, $response->getStatusCode());
 
         $logEntry = $this->getConnectionPool()->getConnectionForTable('tx_formlog_entries')
             ->select(['*'], 'tx_formlog_entries')
             ->fetchAssociative();
 
         self::assertSame(123, $logEntry['page'] ?? null);
-        self::assertSame($formDefinition->getIdentifier(), $logEntry['identifier'] ?? null);
+        self::assertSame($formIdentifier, $logEntry['identifier'] ?? null);
         self::assertSame($expectedData, $logEntry['data'] ?? null);
         self::assertSame('[]', $logEntry['finisher_variables'] ?? null);
     }
 
     public static function formData(): \Generator
     {
-        yield 'basic' => [
-            [
-                [
-                    'identifier' => 'name',
-                    'type' => 'Text',
-                ],
-            ],
+        yield [
+            '1:/form_definitions/Basic.form.yaml',
+            'Basic',
             [
                 'name' => 'Tester',
             ],
+            [],
             '{"name":"Tester"}',
         ];
 
-        yield 'date' => [
-            [
-                [
-                    'identifier' => 'date',
-                    'type' => 'Date',
-                ],
-            ],
+        yield [
+            '1:/form_definitions/Date.form.yaml',
+            'Date',
             [
                 'date' => '2022-02-07',
             ],
+            [],
             '{"date":"07.02.2022"}',
         ];
 
-        yield 'date with custom display format' => [
-            [
-                [
-                    'identifier' => 'date',
-                    'type' => 'Date',
-                    'properties' => [
-                        'displayFormat' => 'Y-m-d',
-                    ],
-                ],
-            ],
+        yield [
+            '1:/form_definitions/DateCustomDisplayFormat.form.yaml',
+            'DateCustomDisplayFormat',
             [
                 'date' => '2022-02-07',
             ],
+            [],
             '{"date":"2022-02-07"}',
         ];
 
@@ -158,26 +134,17 @@ final class LoggerFinisherTest extends FunctionalTestCase
         file_put_contents($temporaryFilePath, 'Test file for upload');
 
         yield 'file upload' => [
+            '1:/form_definitions/FileUpload.form.yaml',
+            'FileUpload',
+            [],
             [
-                [
-                    'identifier' => 'upload',
-                    'type' => 'FileUpload',
-                    'properties' => [
-                        'saveToFileMount' => '1:/',
-                        'allowedMimeTypes' => [
-                            'text/plain',
-                        ],
-                    ],
-                ],
-            ],
-            [
-                'upload' => [
-                    'name' => 'test.txt',
-                    'type' => 'text/plain',
-                    'size' => filesize($temporaryFilePath),
-                    'tmp_name' => $temporaryFilePath,
-                    'error' => UPLOAD_ERR_OK,
-                ],
+                'upload' => new UploadedFile(
+                    input: $temporaryFilePath,
+                    size: 20,
+                    clientFilename: 'test.txt',
+                    clientMediaType: 'text/plain',
+                    errorStatus: \UPLOAD_ERR_OK,
+                ),
             ],
             '{"upload":{"file":{"name":"test.txt"}}}',
         ];
@@ -186,107 +153,53 @@ final class LoggerFinisherTest extends FunctionalTestCase
     #[Test]
     public function logsFinisherVariables(): void
     {
-        $formDefinition = $this->buildFormDefinition([
-            'renderables' => [
-                'page' => [
-                    'renderables' => [
-                        [
-                            'identifier' => 'name',
-                            'type' => 'Text',
-                        ],
-                    ],
-                ],
-            ],
-            'finishers' => [
-                [
-                    'identifier' => 'SaveToDatabase',
-                    'options' => [
-                        'table' => 'pages',
-                        'databaseColumnMappings' => [
-                            'title' => [
-                                'value' => 'Page created by form',
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'identifier' => 'LogFormData',
-                    'options' => [
-                        'finisherVariables' => [
-                            'SaveToDatabase' => [
-                                'insertedUids.0',
-                            ],
-                        ],
-                    ],
-                ],
-            ],
-        ]);
+        $formContentUid = $this->createFormContentElement('1:/form_definitions/FinisherVariables.form.yaml');
+        $formIdentifier = sprintf('FinisherVariables-%d', $formContentUid);
 
-        $this->submitForm($formDefinition, [
-            'name' => 'Tester',
-        ]);
+        $pageRequest = (new InternalRequest())->withPageId(123);
+        $response = $this->executeFrontendSubRequest($pageRequest);
+
+        self::assertSame(200, $response->getStatusCode());
+
+        $formData = (new FormDataFactory())->fromHtmlMarkupAndXpath((string)$response->getBody(), '//form[@id="' . $formIdentifier . '"]');
+        $formSubmitRequest = $formData->with('name', 'Tester')->toPostRequest($pageRequest);
+        $response = $this->executeFrontendSubRequest($formSubmitRequest);
+
+        self::assertSame(200, $response->getStatusCode());
 
         $logEntry = $this->getConnectionPool()->getConnectionForTable('tx_formlog_entries')
             ->select(['*'], 'tx_formlog_entries')
             ->fetchAssociative();
 
         self::assertSame(123, $logEntry['page'] ?? null);
-        self::assertSame($formDefinition->getIdentifier(), $logEntry['identifier'] ?? null);
+        self::assertSame($formIdentifier, $logEntry['identifier'] ?? null);
         self::assertSame('{"name":"Tester"}', $logEntry['data'] ?? null);
         self::assertSame('{"SaveToDatabase":{"insertedUids.0":124}}', $logEntry['finisher_variables'] ?? null);
     }
 
-    protected function buildFormDefinition(array $configuration): FormDefinition
+    private function createFormContentElement(string $formPersistenceIdentifier): int
     {
-        $commonConfiguration = [
-            'prototypeName' => 'standard',
-            'identifier' => 'LoggerFinisherTest',
-            'renderables' => [
-                'page' => [
-                    'identifier' => 'page1',
-                    'type' => 'Page',
-                ],
-            ],
-        ];
-
-        $arrayFormFactory = GeneralUtility::makeInstance(ArrayFormFactory::class);
-        $formDefinition = $arrayFormFactory->build(array_merge_recursive($commonConfiguration, $configuration));
-
-        return $formDefinition;
-    }
-
-    protected function submitForm(FormDefinition $formDefinition, array $formValues = []): void
-    {
-        $formState = GeneralUtility::makeInstance(FormState::class);
-        $formState->setLastDisplayedPageIndex(0);
-
-        foreach ($formValues as $name => $value) {
-            $formState->setFormValue($name, $value);
-        }
-
-        $requestArguments = array_merge(
-            $formValues,
-            [
-                '__currentPage' => 1,
-                '__state' => GeneralUtility::makeInstance(HashService::class)->appendHmac(base64_encode(serialize($formState))),
-                '__session' => GeneralUtility::makeInstance(FormSession::class)->getAuthenticatedIdentifier(),
-            ]
-        );
-
-        $contentObjectRenderer = new ContentObjectRenderer();
-        $contentObjectRenderer->setUserObjectType(ContentObjectRenderer::OBJECTTYPE_USER_INT);
-
-        $requestFactory = GeneralUtility::makeInstance(ServerRequestFactory::class);
-        $serverRequest = $requestFactory->createServerRequest('POST', 'http://localhost')
-            ->withAttribute('applicationType', SystemEnvironmentBuilder::REQUESTTYPE_FE)
-            ->withAttribute('extbase', GeneralUtility::makeInstance(ExtbaseRequestParameters::class))
-            ->withAttribute('currentContentObject', $contentObjectRenderer);
-
-        $request = GeneralUtility::makeInstance(ExtbaseRequest::class, $serverRequest)->withArguments([
-            $formDefinition->getIdentifier() => $requestArguments,
+        $connection = $this->getConnectionPool()->getConnectionForTable('tt_content');
+        $connection->insert('tt_content', [
+            'uid' => 1,
+            'pid' => 123,
+            'CType' => 'form_formframework',
+            'pi_flexform' => <<<XML
+                <?xml version="1.0" encoding="utf-8" standalone="yes" ?>
+                <T3FlexForms>
+                    <data>
+                        <sheet index="sDEF">
+                            <language index="lDEF">
+                                <field index="settings.persistenceIdentifier">
+                                    <value index="vDEF">{$formPersistenceIdentifier}</value>
+                                </field>
+                            </language>
+                        </sheet>
+                    </data>
+                </T3FlexForms>
+            XML,
         ]);
-        $formRuntime = $formDefinition->bind($request);
 
-        $formRuntime->render();
+        return (int)$connection->lastInsertId('tt_content');
     }
 }
